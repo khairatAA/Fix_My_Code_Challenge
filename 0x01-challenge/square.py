c9805de1aedfe105497ed78894ca3d1d1b2a7a29
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-16 15:27:07 +0100

    fix: pycodestyle.

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,6 +8,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """The class constructor"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,9 +17,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """PermiterOfMySquare"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """The string format"""
         return "{}/{}".format(self.width, self.height)
 
 
