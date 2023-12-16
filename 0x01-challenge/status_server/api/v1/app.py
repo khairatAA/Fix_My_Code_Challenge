a70ce22fffe8a3bcbddac3ca9f5463c2406e6d7f
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-16 16:01:36 +0100

    fix: pycodestyle

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Web server 
+Web server
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,9 +15,9 @@ def not_found(error):
     """ json 404 page """
     return make_response(jsonify({"error": "Not found"}), 404)
     # response_text = response.get_data(as_text=True) + '\n'
-    #return response_text
+    # return response_text
 
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
+    # python -m api.v1.app
     app.run(host="0.0.0.0", port=5000)
