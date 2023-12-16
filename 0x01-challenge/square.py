5da600576b2b0fc62c7c5b64a1e0127c3e6e353d
Khairat Adesina <khairatadesina01@gmail.com>
2023-12-16 15:41:14 +0100

    add: code refactoring

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """square module"""
 
 
-class square():
+class Square():
     """ handles a square"""
     width = 0
     height = 0
@@ -14,7 +14,7 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """PermiterOfMySquare"""
@@ -26,7 +26,7 @@ class square():
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
